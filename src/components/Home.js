fbb093731676fe0cbc9b98eecc225a7809d84c28
raych2 <rtongco@gmail.com>
2021-11-14 18:07:36 -0800

    Update Home to include PostPreview and published status

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Spinner from 'react-spinkit';
+import PostPreview from './PostPreview';
+
+const SpinnerContainer = styled.div`
+  margin-top: 200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+`;
+const PostList = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-flow: column wrap;
+  align-content: space-around;
+
+  h1 {
+    margin-bottom: 20px;
+    text-align: center;
+  }
+`;
 
 const Home = ({ userData }) => {
+  const [blogPosts, setBlogPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const blogAPI = 'https://rt-blog-api.herokuapp.com/posts';
+
+  useEffect(() => {
+    async function fetchBlogAPI() {
+      setError(false);
+      setLoading(true);
+      try {
+        const response = await fetch(blogAPI);
+        const data = await response.json();
+        setBlogPosts(data.posts);
+      } catch (err) {
+        console.log(err);
+        setError(true);
+      }
+      setLoading(false);
+    }
+
+    fetchBlogAPI();
+  }, []);
   return (
     <div>
-      <h1>Welcome {userData}!</h1>
+      {error && <div>An error occurred</div>}
+      {loading ? (
+        <SpinnerContainer>
+          <Spinner name="ball-spin-fade-loader" color="#FCA311" />
+        </SpinnerContainer>
+      ) : (
+        <PostList>
+          <h1>Welcome {userData}!</h1>
+          {blogPosts.map((post, index) => {
+            return (
+              <div key={post._id}>
+                <PostPreview post={blogPosts[index]} />
+              </div>
+            );
+          })}
+        </PostList>
+      )}
     </div>
   );
 };
---
+++ src/components/PostPreview.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const PostContainer = styled.div`
+  height: auto;
+  width: 50vw;
+  margin: 10px 0;
+  padding: 20px;
+  background: #ffffff;
+  border: 0.5px solid #7a7a7a;
+  border-radius: 5px;
+
+  .info {
+    max-width: 100%;
+    text-align: left;
+  }
+  h2 {
+    margin: 5px auto;
+  }
+  .author {
+    margin: 5px auto;
+  }
+  .text {
+    margin: 10px auto;
+  }
+  hr {
+    margin: 10px auto;
+    border: none;
+    border-top: 1px solid #000;
+  }
+  .data {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    max-width: 100%;
+    margin: 0 auto;
+  }
+  .published {
+    color: #466ec3;
+  }
+  .unpublished {
+    color: #ca7d02;
+  }
+  @media (max-width: 400px) {
+    width: 100%;
+    .data {
+      flex-direction: column;
+    }
+  }
+`;
+const PostLink = styled(Link)`
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+  color: #000;
+`;
+
+const PostPreview = (props) => {
+  const id = props.post._id;
+  const title = props.post.title;
+  const author = props.post.author.username;
+  const published = props.post.published;
+  const postDate = props.post.postDate;
+  const comments = props.post.comments;
+  const postText = props.post.text.substring(0, 75) + '...';
+
+  return (
+    <>
+      <PostContainer>
+        <div className="info">
+          <PostLink to={`/posts/${id}`}>
+            <h2>{title}</h2>
+          </PostLink>
+          <p className="author">By: {author}</p>
+          <div clasName="text">{postText}</div>
+        </div>
+        <hr />
+        <div className="data">
+          <div>
+            <p>{postDate}</p>
+            {comments.length === 0 ? (
+              <p>No comments</p>
+            ) : comments.length === 1 ? (
+              <p>{comments.length} Comment</p>
+            ) : (
+              <p>{comments.length} Comments</p>
+            )}
+          </div>
+          {published ? (
+            <p className="published">Published</p>
+          ) : (
+            <p className="unpublished">Unpublished</p>
+          )}
+        </div>
+      </PostContainer>
+    </>
+  );
+};
+
+export default PostPreview;
