5842fa01e53710af96a5f5252b0ccf6409412b8c
raych2 <rtongco@gmail.com>
2021-11-28 18:49:18 -0800

    Add NavigationLinks and BurgerMenu

---
+++ src/components/NavigationLinks.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const LinkList = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  li {
+    list-style-type: none;
+    padding: 1rem;
+    &:hover {
+      color: #14213d;
+      background-color: #e5e5e5;
+    }
+  }
+  @media (max-width: 400px) {
+    flex-flow: column nowrap;
+    background-color: #14213d;
+    position: fixed;
+    transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
+    top: 3.5rem;
+    right: 0;
+    height: 15vh;
+    width: 100vw;
+    padding-top: 0;
+    transition: transform 0.3s ease-in-out;
+  }
+`;
+const NavLink = styled(Link)`
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+  display: block;
+  color: #ffffff;
+`;
+
+const NavigationLinks = ({ setAuthorizedUser, open, setOpen }) => {
+  const logOut = (e) => {
+    localStorage.setItem('bearer', '');
+    setAuthorizedUser(false);
+  };
+
+  return (
+    <>
+      <LinkList open={open}>
+        <NavLink to="/posts/create" onClick={() => setOpen(!open)}>
+          <li>Create post</li>
+        </NavLink>
+        <NavLink onClick={logOut} to="/">
+          <li>Log out</li>
+        </NavLink>
+      </LinkList>
+    </>
+  );
+};
+
+export default NavigationLinks;
